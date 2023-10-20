fd20c06ccf7b2534e167e7b78dc9a818434e64d4
Jared Doran <doranj5121@my.uwstout.edu>
2023-10-20 10:35:02 -0500

    Added jsdoc

--- client/passwordGenerator.js
+++ client/passwordGenerator.js
@@ -1,7 +1,11 @@
 // Purpose: Generate a random password for the user
 const { generate } = require('random-words')
 
-// Pass in the number of words you want in the password
+/**
+ * Generates a random password based on a number of words that you pass in
+ * @param {number} numWords - number of words to use in the password. If less than two, it will default to two
+ * @returns password string that will be at least 12 characters long and contain at least one number
+ */
 function generatePassword(numWords) {
     // If the user passes in a word count less than 2, set it to 2
     if (numWords < 2) {
