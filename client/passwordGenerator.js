56846bde0495c9665158b64c6961b66d73068819
Jared Doran <doranj5121@my.uwstout.edu>
2023-10-20 10:28:41 -0500

    Fixed issue with jest testing and added comments / clarified variables

--- client/passwordGenerator.js
+++ client/passwordGenerator.js
@@ -1,11 +1,13 @@
 // Purpose: Generate a random password for the user
 const { generate } = require('random-words')
 
-function generatePassword (length) {
-    if (length < 2) {
-        length = 2
+// Pass in the number of words you want in the password
+function generatePassword(numWords) {
+    // If the user passes in a word count less than 2, set it to 2
+    if (numWords < 2) {
+        numWords = 2
     }
-    let wordArray = generate(length)
+    let wordArray = generate(numWords)
     let tempPass = wordArray.join('-')
     tempPass = tempPass.concat(Math.floor(Math.random() * 10))
     while(tempPass.length < 12) {
