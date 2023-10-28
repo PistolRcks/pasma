dff62112a65e642e4c0e0f48248d6fe7e6b26906
dvcky <colebackman@pm.me>
2023-10-28 16:14:50 -0500

    First implementation of dislikes and testing.

--- server/api/react.ts
+++ server/api/react.ts
@@ -1,51 +1,82 @@
 import { db } from "../database";
 import { Request, Response } from "express";
+import { sessions } from "../types/Session";
 
 let firstPost: boolean = true;
 let postID: number;
 
 /**
  * Reacts to a post.
- * @param {Request} req Requires a JSON body which includes an 'id' field.
+ * @param {Request} req Requires a JSON body which includes an 'id' and 'token' field.
  * @param {Response} res Response to request (200 if success, 400 if post does not exist, and 500 if failed for other reason)
  */
 export function react(req: Request, res: Response) {
-    if ("id" in req.body) {
-        // first, lets make sure the post exists
-        db.get(`SELECT * FROM Posts WHERE ID=?`, [req.body.id], function (err: Error, row: any) {
-            // error running query
-            if (err) {
-                console.log("[SQL] Error: " + err);
-                res.status(500).send("Database error!");
-                return;
-            }
-            // if no post is found
-            if (!row || row.test == "fail") {
-                console.log('[API] Edit request failed! (invalid post ID)');
-                res.status(403).send("Invalid post ID!");
-                return;
-            }
-            // then, check if we have already disliked this post (we want to remove the dislike if that is the case)
-            db.get(`SELECT * FROM PostDislikes WHERE ID=?`, [req.body.id], function (err: Error, row: any) {
-                
-            });
-            // post found, react to post
-            db.run(`UPDATE PostDislikes SET ID=?,  WHERE ID=?`, [req.body.content, req.body.id],
-                function (err: Error) {
+    if ("id" in req.body && "token" in req.body) {
+        // make sure we have a valid token (one that finds us a valid user)
+        if (sessions.has(req.body.token)) {
+            const username = sessions.get(req.body.token).username;
+
+            // first, lets make sure the post exists
+            db.get(`SELECT * FROM Posts WHERE ID=?`, [req.body.id], function (err: Error, postRow: any) {
+                // error running query
+                if (err) {
+                    console.log("[SQL] Error: " + err);
+                    res.status(500).send("Database error!");
+                    return;
+                }
+
+                // if no post is found
+                if (!postRow || !postRow.test_validpost) {
+                    console.log('[API] Edit request failed! (invalid post ID)');
+                    res.status(400).send("Invalid post ID!");
+                    return;
+                }
+
+                // then, check if we have already disliked this post (we want to remove the dislike if that is the case)
+                db.get(`SELECT * FROM PostDislikes WHERE ID=? AND Username=?`, [req.body.id, username], function (err: Error, dislikeRow: any) {
                     if (err) {
                         console.log("[SQL] Error: " + err);
                         res.status(500).send("Database error!");
                         return;
                     }
-                    console.log('[API] Post ' + req.body.id + ' editted');
-                    res.status(200).send();
+                    if (!dislikeRow || !dislikeRow.test_disliked) {
+                        // no dislike yet, we can dislike this post
+                        db.run(`INSERT INTO PostDislikes VALUES (?, ?)`, [req.body.id, username], function (err: Error) {
+                            if (err) {
+                                console.log("[SQL] Error: " + err);
+                                res.status(500).send("Database error!");
+                                return;
+                            }
+                            // successfully disliked post
+                            console.log("[API] " + username + " disliked post " + req.body.id);
+                            res.status(200).send();
+                        });
+                        return;
+                    }
+                    // post already disliked! remove dislike from the post
+                    db.run(`DELETE FROM PostDislikes WHERE ID=? AND Username=?`, [req.body.id, username], function (err: Error) {
+                        if (err) {
+                            console.log("[SQL] Error: " + err);
+                            res.status(500).send("Database error!");
+                            return;
+                        }
+                        // successfully removed dislike from post
+                        console.log("[API] " + username + " removed dislike from post " + req.body.id);
+                        res.status(200).send();
+                    });
                     return;
                 });
-        });
+            });
+        } else {
+            // invalid user token
+            console.log("[API] Dislike request failed! (invalid token provided)");
+            res.status(500).send("Invalid token provided!");
+            return;
+        }
     } else {
-        //invalid request data
-        console.log('[API] Edit request failed! (invalid edit request)');
-        res.status(500).send("Invalid edit request!");
+        // invalid request data
+        console.log('[API] Dislike request failed! (invalid dislike request)');
+        res.status(500).send("Invalid dislike request!");
         return;
     }
 }
--- test/server/api/react.test.ts
+++ test/server/api/react.test.ts
@@ -1,39 +1,49 @@
 import supertest from "supertest";
 import app from "../../../server/server";
 import { db } from "../../../server/database";
-import { DBGetType, DBGetTypeWithParams, DBRunTypeWithCallback } from "../../utils/types";
+import { DBGetTypeWithParams, DBRunTypeWithCallback } from "../../utils/types";
+import { addSession } from "../../../server/types/Session";
 jest.mock('../../../server/database');
+
+// instead of actually reflecting changes in the database, we simply keep track of the
+// a post ID and token that is the "post" we are reacting to
+let postID = "0";
+let bobsToken = addSession({ username: "bob" });
+let bobDisliked = false;
 
 const fakeReact = {
     "hehe": "hawhaw"
 }
 
 let realReact = {
-    "id": "0"
+    "id": "0",
+    "token": bobsToken
 }
 
 let realReactBadID = {
-    "id": "1234"
+    "id": "1234",
+    "token": bobsToken
 }
 
-// instead of actually reflecting changes in the database, we simply keep track of the
-// a boolean and ID that is the "post" we are reacting to
-let postLiked = false;
-let postID = "0";
+let realReactBadToken = {
+    "id": "0",
+    "token": "imtryingtohackbobsminecraftaccount"
+}
 
 const mockGet = jest.fn((stmt, params, callback) => {
-    // nothing super specific needed here, just can't be empty
-    let row = { test: "pass" };
-    // however, if the ID isn't valid, change it to something the code can recognize as a fail
+    let row = {
+        test_validpost: true,
+        test_disliked: bobDisliked
+    };
     if (params[0] != postID) {
-        row = { test: "fail" };
+        row.test_validpost = false;
     }
     // @ts-ignore
     callback(null, row);
 }) as jest.MockedFunction<DBGetTypeWithParams>;
 
 const mockRun = jest.fn((stmt, params, callback) => {
-    postContent = params[0];
+    bobDisliked = !bobDisliked;
     // @ts-ignore
     callback(null);
 }) as jest.MockedFunction<DBRunTypeWithCallback>;
@@ -50,10 +60,10 @@ const mockRunError = jest.fn((stmt, params, callback) => {
 }) as jest.MockedFunction<DBRunTypeWithCallback>;
 
 // block console logging so it doesn't get annoying
-beforeAll(() => {
-    jest.spyOn(console, "log").mockImplementation();
-    jest.spyOn(console, "error").mockImplementation();
-})
+// beforeAll(() => {
+//     jest.spyOn(console, "log").mockImplementation();
+//     jest.spyOn(console, "error").mockImplementation();
+// });
 
 describe('[API] /edit: database', () => {
 
@@ -61,30 +71,29 @@ describe('[API] /edit: database', () => {
         db.get = jest.fn();
         db.run = jest.fn();
 
-        // need to reset this for each test
-        postContent = "uneditted message";
+        bobDisliked = false;
     });
 
     test("'get' error", async () => {
         db.get = mockGetError;
         db.run = mockRun;
 
-        const res = await supertest(app).post("/api/edit").send(realEdit);
+        const res = await supertest(app).post("/api/react").send(realReact);
 
         expect(res.status).toBe(500);
         expect(res.text).toBe("Database error!");
-        expect(postContent).toBe("uneditted message");
+        expect(bobDisliked).toBeFalsy();
     });
 
     test("'run' error", async () => {
         db.get = mockGet;
         db.run = mockRunError;
 
-        const res = await supertest(app).post("/api/edit").send(realEdit);
+        const res = await supertest(app).post("/api/react").send(realReact);
 
         expect(res.status).toBe(500);
         expect(res.text).toBe("Database error!")
-        expect(postContent).toBe("uneditted message");
+        expect(bobDisliked).toBeFalsy();
     });
 });
 
@@ -94,40 +103,68 @@ describe('[API] /post: request', () => {
         db.get = jest.fn();
         db.run = jest.fn();
 
-        // need to reset this for each test
-        postContent = "uneditted message";
     });
 
-    test("Test valid edit", async () => {
+    test("Test add dislike", async () => {
         db.get = mockGet;
         db.run = mockRun;
 
-        const res = await supertest(app).post("/api/edit").send(realEdit);
+        bobDisliked = false;
+
+        const res = await supertest(app).post("/api/react").send(realReact);
 
         expect(res.status).toBe(200);
         expect(res.text).toBe("");
-        expect(postContent).toBe("editted message!");
+        expect(bobDisliked).toBe(true);
+    });
+
+    test("Test remove dislike", async () => {
+        db.get = mockGet;
+        db.run = mockRun;
+
+        bobDisliked = true;
+
+        const res = await supertest(app).post("/api/react").send(realReact);
+
+        expect(res.status).toBe(200);
+        expect(res.text).toBe("");
+        expect(bobDisliked).toBe(false);
     });
 
     test("Test invalid post ID", async () => {
         db.get = mockGet;
         db.run = mockRun;
 
-        const res = await supertest(app).post("/api/edit").send(realEditBadID);
+        bobDisliked = false;
 
-        expect(res.status).toBe(403);
+        const res = await supertest(app).post("/api/react").send(realReactBadID);
+
+        expect(res.status).toBe(400);
         expect(res.text).toBe("Invalid post ID!");
-        expect(postContent).toBe("uneditted message");
+        expect(bobDisliked).toBe(false);
+    });
+
+    test("Test invalid user token", async () => {
+        db.get = mockGet;
+        db.run = mockRun;
+
+        bobDisliked = false;
+
+        const res = await supertest(app).post("/api/react").send(realReactBadToken);
+
+        expect(res.status).toBe(500);
+        expect(res.text).toBe("Invalid token provided!");
+        expect(bobDisliked).toBe(false);
     });
 
     test("Test invalid request", async () => {
         db.get = mockGet;
         db.run = mockRun;
 
-        const res = await supertest(app).post("/api/edit").send(fakeEdit);
+        const res = await supertest(app).post("/api/react").send(fakeReact);
 
         expect(res.status).toBe(500);
-        expect(res.text).toBe("Invalid edit request!");
-        expect(postContent).toBe("uneditted message");
+        expect(res.text).toBe("Invalid dislike request!");
+        expect(bobDisliked).toBe(false);
     });
 });
