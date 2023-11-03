59178548ca81e36c8344a26a6a5b0552266373bf
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2023-11-03 10:28:41 -0500

    Finished testing

--- server/api/feed.ts
+++ server/api/feed.ts
@@ -6,7 +6,7 @@ import { sessions } from "../types/Session";
  * Returns a list of the (default) top 100 most recent posts visible to the currently logged in user
  * @param {Request} req - Requests a JSON object in the body containing:
  *          - "token": valid user token
- *          - "size": the maximum amount of most recent posts to read (optional, default 100)
+ *          - "size": the maximum amount of most recent posts to read (optional, default 100; no less than 1 and no greater than 10000)
  *          - "bookmark": the post ID of the last post in the previous page (-1 if grabbing the first page; x
  *              optional, unimplemented)
  * @param {Response} res - Responds with errortext in the case of a user or internal 
@@ -21,71 +21,81 @@ import { sessions } from "../types/Session";
  *      friends of that user.
  */
 export function feed(req: Request, res: Response) {
-    if (typeof req.body === "object") {
-        // ensure valid request 
-        if (!("token" in req.body)) {
-            res.status(400).send("Error: \"token\" not in request JSON.");
-            return;
-        }
-        
-        // ensure logged in
-        if (!sessions.has(req.body.token)) {
-            // invalid user token
-            console.log("[API] Feed get failed! (invalid token provided)");
-            res.status(401).send("Invalid token provided!");
-            return;
-        }
-        
-        let size = 100;
-        if ("size" in req.body) {
-            if (typeof req.body.size !== "number") {
-                res.status(400).send("Error: \"size\" is not of type \"number\" in request JSON.");
-                return;
-            } else {
-                size = req.body.size;
-            }
-        }
-        
-        // get posts
-        /** 
-         * WHAT IS A POST? Nothing but a miserable little pile of:
-         * id: post ID
-         * user: user that created the post
-         * picture: the filename of the image attached to the post, if any
-         * content: contents of the message, if any
-         * dislikes: number of dislikes on the post
-         * comments: number of comments on the post (unimplemented; will work on that later)
-        */
-        db.all(`
-            SELECT 
-                p.ID as id, 
-                p.Username as user, 
-                p.Timestamp as timestamp,
-                p.Picture as picture, 
-                p.Content as content,
-                (
-                    SELECT count(*)
-                    FROM PostDislikes pd
-                    WHERE 
-                        pd.ID = p.ID AND
-                        pd.Disliked = true
-                ) as dislikes
-            FROM Posts p
-            ORDER BY 
-                timestamp DESC,
-                id DESC
-            LIMIT ${size};
-        `, function (err, rows) {
-            if (err) {
-                console.log("[SQL] Error: " + err);
-                res.status(500).send("Database error!");
-                return;
-            } 
-            
-            res.status(200).send(rows);
-        })
-    } else {
-        res.status(400).send("Error: Request body was not able to be transformed into JSON.");
+    // ensure valid request 
+    if (!("token" in req.body)) {
+        res.status(400).send("Error: \"token\" not in request JSON.");
         return;
     }
+    
+    // ensure logged in
+    if (!sessions.has(req.body.token)) {
+        // invalid user token
+        console.log("[API] Feed get failed! (invalid token provided)");
+        res.status(401).send("Error: Invalid token provided.");
+        return;
+    }
+    
+    let size = 100;
+    if ("size" in req.body) {
+        if (typeof req.body.size !== "number") {
+            res.status(400).send("Error: \"size\" is not of type \"number\" in request JSON.");
+            return;
+        } else if (req.body.size < 1) {
+            res.status(400).send("Error: \"size\" cannot be less than 1.");
+            return;
+        } else if (req.body.size > 10000) {
+            res.status(400).send("Error: \"size\" cannot be greater than 10000.");
+            return;
+        } else {
+            size = req.body.size;
+        }
+    }
+    
+    // get posts
+    /** 
+     * WHAT IS A POST? Nothing but a miserable little pile of:
+     * id: post ID
+     * user: user that created the post
+     * picture: the filename of the image attached to the post, if any
+     * content: contents of the message, if any
+     * dislikes: number of dislikes on the post
+     * comments: number of comments on the post (unimplemented; will work on that later)
+    */
+    db.all(`
+        SELECT 
+            p.ID as id, 
+            p.Username as user, 
+            p.Timestamp as timestamp,
+            p.Picture as picture, 
+            p.Content as content,
+            (
+                SELECT count(*)
+                FROM PostDislikes pd
+                WHERE 
+                    pd.ID = p.ID AND
+                    pd.Disliked = true
+            ) as dislikes
+        FROM Posts p
+        ORDER BY 
+            timestamp DESC,
+            id DESC
+        LIMIT ?;
+    `, 
+    [size],
+    function (err, rows) {
+        if (err) {
+            console.log("[SQL] Error: " + err);
+            res.status(500).send("Error: Database error!");
+            return;
+        } 
+        
+        // If we get no rows, return an empty list so that the frontend
+        // can keep working smoothly
+        // Otherwise it will be an empty object which may not be nice
+        if (!rows) {
+            rows = [];
+        }
+        
+        res.status(200).send(rows);
+    });
 }
