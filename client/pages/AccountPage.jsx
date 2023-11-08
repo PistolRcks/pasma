722438eedfdc88ff2859a788485274ff872c02a2
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2023-11-08 10:41:27 -0600

    Tests

--- client/pages/AccountPage.jsx
+++ client/pages/AccountPage.jsx
@@ -4,10 +4,10 @@ const React = require('react');
  * Renders the account management page.
  * @param {object} props - Unused.
  */
-function PostFeedPage(props) {
+function AccountPage(props) {
     return (
         <p>{`This page is "/account" (the account management page).`}</p>
     );
 }
 
-module.exports = PostFeedPage;
+module.exports = AccountPage;
