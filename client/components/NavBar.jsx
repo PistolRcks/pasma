69de96d4aa899488c6bf5cc67211271e6a292890
Jared Doran <doranj5121@my.uwstout.edu>
2023-11-10 11:38:56 -0600

    Coded Nav Bar frame

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,1 +1,48 @@
-//Yay!
+const React = require('react')
+const { useState, useEffect } = require('react')
+const { retrieveProfilePicture } = require('../dataHelper.js')
+const PropTypes = require('prop-types')
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+function NavBar (props) {
+    return (
+        <>
+            <Navbar isBordered>
+                <NavbarBrand>
+                    <img src="/pictures/logos/pasmaSquare.png" alt="PASMA" className="w-12 h-12 mr-2" />
+                    <p className="font-bold text-inherit">PASMA</p>
+                </NavbarBrand>
+                <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Feed
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Profile
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            Cookie Viewer
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
+                <NavbarContent justify="end">
+                    <NavbarItem className="hidden lg:flex">
+                    <Link href="#">Login</Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                    <Button as={Link} color="primary" href="#" variant="flat">
+                        Sign Up
+                    </Button>
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
+      
+        </>
+    )
+}
+
+module.exports = NavBar;
