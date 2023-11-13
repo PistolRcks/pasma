136eab5063d091a83cf726af901ac2d67ee84467
dvcky <colebackman@pm.me>
2023-11-13 10:42:19 -0600

    first commit for implementation

---
+++ server/api/getPhrases.ts
@@ -0,0 +1,38 @@
+import { db } from '../database';
+import { Request, Response } from 'express';
+import { sessions } from "../types/Session";
+
+/**
+ * Gets a list of possible post phrases from the database.
+ * @param {Request} req Requires a JSON body which includes 'token' field.
+ * @param {Response} res Response to request (200 if success, 401 if token is invalid, and 500 if failed for other reason)
+ */
+export function getPhrases(req: Request, res: Response) {
+
+    if (!("token" in req.body)) {
+        console.log('[API] getPhrases request failed! (invalid getPhrases request)');
+        res.status(500).send("Invalid getPhrases request!");
+        return;
+    }
+
+    if (!sessions.has(req.body.token)) {
+        console.log("[API] getPhrases request failed! (invalid token provided)");
+        res.status(401).send("Invalid token provided!");
+        return;
+    }
+
+    db.all(`SELECT Phrase FROM 'PostPhrases'`, function (err, rows: any) {
+        if (err) {
+            console.log("[SQL] Error: " + err);
+            res.status(500).send("Database error!");
+            return;
+        }
+
+        if (!rows) {
+            rows = [];
+        }
+
+        res.status(200).send(rows);
+        return;
+    });
+};
---
+++ test/server/api/getPhrases.test.ts
@@ -0,0 +1,98 @@
+import supertest from "supertest";
+import app from "../../../server/server";
+import { db } from "../../../server/database";
+import { DBAllType } from "../../utils/types";
+import { addSession } from "../../../server/types/Session";
+
+jest.mock("../../../server/database");
+
+const req = supertest(app);
+let bobsToken = addSession({ username: "bob" });
+
+// this is a simple example of how the output from the request is structured
+let rows =
+    [
+        {
+            "Phrase": "first phrase"
+        },
+        {
+            "Phrase": "second phrase"
+        }
+    ];
+
+let realGetPhrases = {
+    "token": bobsToken
+};
+
+let realGetPhrasesBadToken = {
+    "token": "ummmmnope"
+};
+
+let fakeGetPhrases = {
+    "invalid": "info"
+}
+
+let mockAll = jest.fn((stmt, callback) => {
+    // @ts-ignore
+    callback(null, rows);
+}) as jest.MockedFunction<DBAllType>;
+
+let mockAllError = jest.fn((stmt, callback) => {
+    // @ts-ignore
+    callback(new Error(), null);
+}) as jest.MockedFunction<DBAllType>;
+
+// block console logging so it doesn't get annoying
+// also generate session token
+let sessionToken: string;
+beforeAll(() => {
+    jest.spyOn(console, "log").mockImplementation();
+    jest.spyOn(console, "error").mockImplementation();
+
+    sessionToken = addSession({ username: "bob" });
+});
+
+describe("[API] /getPhrases: database", () => {
+
+    beforeEach(() => {
+        db.all = jest.fn();
+    });
+
+    test("'all' error", async () => {
+        db.all = mockAllError;
+
+        const res = await supertest(app).get("/api/getPhrases").send(realGetPhrases);
+
+        expect(res.status).toBe(500);
+        expect(res.text).toBe("Database error!");
+    });
+
+})
+
+describe('[API] /getPhrases: request', () => {
+
+    beforeEach(() => {
+        db.all = mockAll;
+    });
+
+    test("Test invalid getPhrases", async () => {
+        const res = await supertest(app).get("/api/getPhrases").send(fakeGetPhrases);
+
+        expect(res.status).toBe(500);
+        expect(res.text).toBe("Invalid getPhrases request!");
+    });
+
+    test("Test valid getPhrases", async () => {
+        const res = await supertest(app).get("/api/getPhrases").send(realGetPhrases);
+
+        expect(res.status).toBe(200);
+        expect(res.text).toEqual(JSON.stringify(rows));
+    });
+
+    test("Test proper getPhrases with bad token", async () => {
+        const res = await supertest(app).get("/api/getPhrases").send(realGetPhrasesBadToken);
+
+        expect(res.status).toBe(401);
+        expect(res.text).toBe("Invalid token provided!");
+    });
+});
