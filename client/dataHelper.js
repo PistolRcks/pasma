cbbbc1f8337134cd3a8e4ca9b6d3e6df91fd3297
Jared Doran <doranj5121@my.uwstout.edu>
2023-11-15 11:38:03 -0600

    Added logOut function

--- client/dataHelper.js
+++ client/dataHelper.js
@@ -26,8 +26,16 @@ export async function retrieveProfilePicture (username) {
       console.error(err)
       return null
     }
-  }
+}
 
+/**
+ * Sends updated password to server
+ * 
+ * @param {*} token 
+ * @param {*} oldPass 
+ * @param {*} newPass 
+ * @returns 
+ */
 export async function sendUpdatedPassword (token, oldPass, newPass) {
     try {
         // Send an AJAX request
@@ -54,6 +62,41 @@ export async function sendUpdatedPassword (token, oldPass, newPass) {
     } catch (err) {
         // something went wrong so return null
         console.error('Failed to update password')
+        console.error(err)
+        return null
+    }
+}
+
+/**
+ * Logs a user out
+ * 
+ * @param {*} token 
+ * @returns 
+ */
+export async function logOut (token) {
+    try {
+        // Send an AJAX request
+        const response = await fetch(`api/logout`, {
+          method: "POST",
+          body: JSON.stringify({
+            token: token
+          }),
+          headers: {
+              "Content-type": "application/json; charset=UTF-8"
+          }
+        })
+  
+        if (response.status >= 400) {
+          throw new Error(`Error ${response.status} - Logout failed`)
+        }
+        else {
+          // Return the response
+          return await response.text()
+        }
+  
+    } catch (err) {
+        // something went wrong so return null
+        console.error('Failed to logout')
         console.error(err)
         return null
     }
