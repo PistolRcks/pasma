0435c3e716ee3bd0ebdab24a5b9ba6f927bc2a6d
Karsten Roland <47155699+KarstenRoland@users.noreply.github.com>
2023-11-17 19:00:47 -0600

    Additional formatting

--- client/components/CreatePostForm.jsx
+++ client/components/CreatePostForm.jsx
@@ -1,6 +1,6 @@
 const React = require('react')
 const { useNavigate } = require('react-router-dom')
-const { Button, Card, CardBody, CardHeader, CardFooter, Image, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Textarea, Tooltip, useDisclosure } = require('@nextui-org/react')
+const { Button, Card, CardBody, CardHeader, CardFooter, Image, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Skeleton, Textarea, Tooltip, useDisclosure } = require('@nextui-org/react')
 const { ArrowBendUpLeft, FileImage, PencilSimple, PencilSimpleLine, X } = require('@phosphor-icons/react')
 const PostModalCard = require('./PostModalCard')
 const ImageIcon = require('@phosphor-icons/react').Image // Alias for Phosphor Icons "Image", since it shares the same name as NextUI's "Image"
@@ -15,7 +15,17 @@ function CreatePostForm (props) {
 
     const {isOpen, onOpen, onOpenChange} = useDisclosure()
 
-    const imagePath = "pictures/stock_images"
+    const imagePath = "pictures/stock_images/"
+
+    const selectPhrase = (phraseString) => {
+        setPhrase(phraseString)
+        onOpenChange()
+    }
+    
+    const selectPicture = (imageURL) => {
+        setPicture(imageURL)
+        onOpenChange()
+    }
 
     const stockPhrases = [ // TODO: Replace with actual phrases
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat diam ut venenatis tellus in.",
@@ -43,7 +53,7 @@ function CreatePostForm (props) {
 
     return (
         <div className="grid justify-items-center mt-6">
-            <Card className="flex-auto w-2/3">
+            <Card className="flex-auto w-1/2">
                 <CardHeader>
                     <Button size="sm" radius="full" startContent={<ArrowBendUpLeft className="h-4 w-4"/>} onClick={() => {
                         navigateTo("/") // TODO: Change to correct route when post feed is implemented
@@ -52,20 +62,42 @@ function CreatePostForm (props) {
                     </Button>
                 </CardHeader>
                 <CardBody>
-                    <Textarea isReadOnly className="cursor-pointer" minRows={8} size="lg" radius="none" placeholder="Click here to select a predefined phrase to post." onClick={async () => {
-                        await setModalState(true)
-                        onOpen()
-                    }}/>
-                </CardBody>
-                <CardFooter className="flex justify-between">
-                    <Tooltip placement="bottom" content="Attach Photo">
-                        <Button isIconOnly radius="full" onPress={async () => {
-                            await setModalState(false)
+                    <Textarea
+                        isReadOnly 
+                        className="cursor-pointer"
+                        minRows={8}
+                        size="lg"
+                        radius="none"
+                        value={phrase}
+                        placeholder="Propagate passive aggression responsibly!"
+                        description="Click the field above to select a predefined phrase for your post."
+                        onClick={async () => {
+                            await setModalState(true)
                             onOpen()
-                        }}>
-                            <ImageIcon className="h-6 w-6"/>
-                        </Button>
-                    </Tooltip>
+                        }}
+                    />
+                </CardBody>
+                <CardFooter className="flex justify-between items-end">
+                    <div className="grid gap-x-0.5 grid-cols-2">
+                        {picture ? <Image className="h-16 w-16" src={imagePath + picture}/> : <></>}
+                        <Tooltip placement={picture ? "right" : "top"} content={picture ? "Remove Picture" : "Attach Picture"}>
+                            <Button
+                                isIconOnly
+                                size={picture ? "sm" : "md"}
+                                radius="full"
+                                onPress={async () => {
+                                    if(picture) {
+                                        setPicture(undefined)
+                                    } else {
+                                        await setModalState(false)
+                                        onOpen()
+                                    }
+                                }}
+                            >
+                                {picture ? <X className="h-4 w-4"/> : <ImageIcon className="h-6 w-6"/>}
+                            </Button>
+                        </Tooltip>
+                    </div>
                     <Button color="primary" radius="full" endContent={<PencilSimple className="h-6 w-6"/>}>Create Post</Button>
                 </CardFooter>
             </Card>
@@ -76,40 +108,19 @@ function CreatePostForm (props) {
                     </ModalHeader>
                     <ModalBody>
                         {modalState ?
-                            <div className="columns-2 gap-x-0 gap-y-2">
+                            <div className="columns-2">
                                 {stockPhrases.map((item, index) => (
-                                    <PostModalCard phraseString={item}/>
+                                    <PostModalCard phraseString={item} sendProperty={selectPhrase}/>
                                 ))}
                             </div>
                             :
-                            <div className="columns-3 gap-x-0 gap-y-2">
+                            <div className="columns-3">
                                 {stockImages.map((item, index) => (
-                                    <PostModalCard imageURL={item}/>
+                                    <PostModalCard imageURL={item} sendProperty={selectPicture}/>
                                 ))}
                             </div>
-                        }
-                        {picture ?
-                            <React.Fragment>
-                                <Image width={50} src={imagePath + picture}/>
-                                <Button isIconOnly><X className="h-0.5 w-0.5"/></Button>
-                            </React.Fragment>
-                            :
-                            <></>
                         }
                     </ModalBody>
-                    <ModalFooter>
-                        {modalState ? 
-                            <Button color="primary" radius="full" onClick={() => {
-                                onOpenChange()
-                                // TODO: Insert phrase
-                            }} endContent={<PencilSimpleLine className="h-6 w-6"/>}>Select Phrase</Button>
-                            :
-                            <Button color="primary" radius="full" onClick={() => {
-                                onOpenChange()
-                                //TODO: Insert photo
-                            }} endContent={<FileImage className="h-6 w-6"/>}>Attach Photo</Button>
-                        }
-                    </ModalFooter>
                 </ModalContent>
             </Modal>
         </div>
--- client/components/PostModalCard.jsx
+++ client/components/PostModalCard.jsx
@@ -1,23 +1,27 @@
 const React = require('react')
 const PropTypes = require('prop-types')
-const { Card, CardBody, Image } = require('@nextui-org/react')
+const { Button, Card, CardBody, CardHeader, CardFooter, Image } = require('@nextui-org/react')
+const { X } = require('@phosphor-icons/react')
 
 function PostModalCard (props) {
-    const { phraseString, imageURL } = props
+    const { phraseString, imageURL, sendProperty } = props
 
     return (
-        <Card className="cursor-pointer">
-            <CardBody>
-                { phraseString != null ? 
-                    <p>{phraseString}</p>
-                    : 
-                    <Image
-                        isPressable
-                        src={"pictures/stock_images/" + imageURL}
-                    />
-                }
-            </CardBody>
-        </Card>
+        <div className="pb-4">
+            <Card isPressable isFooterBlurred className="cursor-pointer" onPress={() => phraseString ? sendProperty(phraseString) : sendProperty(imageURL)}>
+                <CardBody>
+                    { phraseString != null ? 
+                        <p>{phraseString}</p>
+                        : 
+                        <Image
+                            isPressable
+                            src={"pictures/stock_images/" + imageURL}
+                        />
+                    }
+                </CardBody>
+            </Card>
+            
+        </div>
     )
 }
 
