c6b8ec8b7074f01199e595f274ca30617f95e0c1
Jared Doran <doranj5121@my.uwstout.edu>
2023-12-06 10:38:19 -0600

    Copied some of karstens code in... still a work in progress

--- client/components/AccountSettingsForm.jsx
+++ client/components/AccountSettingsForm.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const { useNavigate } = require('react-router-dom')
 const { useCookies } = require('react-cookie')
-const { Button, Card, CardBody, CardHeader, CardFooter, Image, Modal, ModalBody, ModalContent, ModalHeader, Spinner, Textarea, Tooltip, useDisclosure } = require('@nextui-org/react')
+const { Button, Card, CardBody, CardHeader, CardFooter, Image, Modal, ModalBody, ModalContent, ModalHeader, Spinner, Textarea, Tooltip, Popover, PopoverTrigger, PopoverContent, Skeleton, useDisclosure } = require('@nextui-org/react')
 const { ArrowBendUpLeft, PencilSimple, X } = require('@phosphor-icons/react')
 const ChangePassword = require('./ChangePassword.jsx')
 
@@ -12,9 +12,21 @@ const ChangePassword = require('./ChangePassword.jsx')
 function AccountSettingsForm (props) {
     const navigateTo = useNavigate()
     const [cookies] = useCookies(["token", "username"])
+    const [profilePictures, setProfilePictures] = React.useState([]);
+    const [profilePicture, setProfilePicture] = React.useState("botttsNeutral-1695826814739.png")
+    const [isPopoverOpen, setIsPopoverOpen] = React.useState(false)
     // const [modalState, setModalState] = React.useState(true) //* true renders phrases, false renders photos
 
     // const {isOpen, onOpen, onOpenChange} = useDisclosure()
+
+    const imagePath = "pictures/profile_pictures/"
+
+    React.useEffect(() => {
+        onStart()
+    }, [])
+    async function onStart() {
+        setProfilePictures(await getAllProfilePictures())
+    }
 
     return (
         <div className="grid justify-items-center mt-6">
@@ -32,6 +44,33 @@ function AccountSettingsForm (props) {
                     </Button>
                 </CardHeader>
                 <CardBody>
+                    <div className="pl-2 pr-6 w-48">
+                        <Popover showArrow isOpen={isPopoverOpen} onOpenChange={(open) => setIsPopoverOpen(open)} placement="right">
+                            <PopoverTrigger>
+                                <Skeleton className="rounded-full" isLoaded={profilePictures.length > 0}>
+                                    <Image className="cursor-pointer" src={imagePath + profilePicture} width={180} radius="full" />
+                                </Skeleton>
+                            </PopoverTrigger>
+                            <PopoverContent className="w-80 max-h-80">
+                                    {profilePictures.length > 0 ?
+                                        <div className="grid grid-cols-3 gap-4 py-3 pr-4 overflow-y-scroll">
+                                            {profilePictures.map((item, index) => (
+                                                <Image key={item} onClick={() => {
+                                                    if(!isFormDisabled) {
+                                                        setProfilePicture(item)
+                                                        setIsPopoverOpen(false)
+                                                    }
+                                                }} className="cursor-pointer" src={imagePath + item} width={100} radius="full" />
+                                            ))}
+                                        </div>
+                                        :
+                                        <div className="py-5 flex items-center"><Spinner color="warning"/><p className="pl-2">Loading profile pictures...</p></div>
+                                        
+                                    }
+                            </PopoverContent>
+                        </Popover>
+                        <p className="text-center pt-2">Click on the photo above to select a different profile picture.</p>
+                    </div>
                     <ChangePassword username={cookies.username} />
                 </CardBody>
                 <CardFooter className="flex justify-between items-end">
